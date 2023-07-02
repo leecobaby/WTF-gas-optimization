5d35d110b96f1e0cebc6cc621b29d7898ab10f7a
Leeco <leeco19@qq.com>
2023-07-02 18:09:03 +0800

    p16

--- 16_MethodIdSort/MethodIdSort.sol
+++ 16_MethodIdSort/MethodIdSort.sol
@@ -2,17 +2,11 @@
 pragma solidity ^0.8.13;
 
 contract MethodIdSort {
-    function test1() external {
-    }
+    function test_y2K() external {}
 
-    function test2() external {
-    }
+    function test1() external {}
 
-    function test3() external {
-    }
+    function test2() external {}
 
-    function test_y2K() external {
-    }
+    function test3() external {}
 }
-
-
